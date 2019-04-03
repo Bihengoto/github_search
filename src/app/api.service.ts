da17ee46b6cffa4c6e55ee1157900618c6464870
Bihengoto <isaacbihengoto@gmail.com>
2019-04-03 08:16:06 +0300

    fixed a bug in the ts file and added the core elements and deatils in the service file

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,28 +1,28 @@
-import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {environment} from './../environments/environment'
-
-@Injectable({
-  providedIn: 'root'
-})
-export class ApiService {
-  userName: string;
-  apiUrl= "https://api.github.com/users/"
-
-
-  constructor(public http: HttpClient) {
-  this.userName = "Bihengoto";
-  }
-
-  getUserDetails() {
-    return this.http.get(this.apiUrl + this.userName )
-  }
-  getRepos()
-  {
-    return this.http.get(this.apiUrl + this.userName )
-  }
-  updateUserName(userName: string) {
-    this.userName = userName;
-
-  }
-}
+// import { Injectable } from '@angular/core';
+// import {HttpClient} from '@angular/common/http';
+// import {environment} from './../environments/environment'
+//
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class ApiService {
+//   userName: string;
+//   apiUrl= "https://api.github.com/users/"
+//
+//
+//   constructor(public http: HttpClient) {
+//   this.userName = "Bihengoto";
+//   }
+//
+//   getUserDetails() {
+//     return this.http.get(this.apiUrl + this.userName )
+//   }
+//   getRepos()
+//   {
+//     return this.http.get(this.apiUrl + this.userName )
+//   }
+//   updateUserName(userName: string) {
+//     this.userName = userName;
+//
+//   }
+// }
