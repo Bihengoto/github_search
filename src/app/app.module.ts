570752c77a6061bc5bf81c8d3a8359663d3c9acb
Bihengoto <isaacbihengoto@gmail.com>
2019-04-03 07:36:25 +0300

    fixed a few bugs in the respective files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,3 @@
-// import { BrowserModule } from '@angular/platform-browser';
-// import { NgModule } from '@angular/core';
-//
-// import { AppRoutingModule } from './app-routing.module';
-// import { AppComponent } from './app.component';
-// import { GithubComponent } from './github/github.component';
-// import { ReposComponent } from './repos/repos.component';
-//
-// @NgModule({
-//   declarations: [
-//     AppComponent,
-//     GithubComponent,
-//     ReposComponent
-//   ],
-//   imports: [
-//     BrowserModule,
-//     AppRoutingModule
-//   ],
-//   providers: [],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule { }
-//
-//
-
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
